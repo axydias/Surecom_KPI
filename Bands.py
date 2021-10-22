31ad3b5a388f9ed8e984bd6d91b858c4b26d6c01
Alexios Xydias <condoral@gmail.com>
2021-10-22 17:19:29 +0300

    Correct 3g bands

--- Bands.py
+++ Bands.py
@@ -3,6 +3,6 @@ from collections import OrderedDict
 Operators = ['Three', 'Eir', 'Vodafone']
 
 Bands3G = OrderedDict()
-Bands3G['Three'] = ['2988', '3010', '3088', '10564', '10589', '10613', '10787', '10812', '10837']
+Bands3G['Three'] = ['2988', '3010', '3088', '10564', '10589', '10613']
 Bands3G['Eir'] = ['2962', '10637', '10662', '10687']
 Bands3G['Vodafone'] = ['3063', '10712', '10737', '10762']
