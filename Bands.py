c445f1ff48701cadb198d274902c46a92c3a4c78
xydias <alexios.xydias@nokia.com>
2023-03-10 09:54:54 +0200

    New bands

--- Bands.py
+++ Bands.py
@@ -3,6 +3,6 @@ from collections import OrderedDict
 Operators = ['Three', 'Eir', 'Vodafone']
 
 Bands4G = OrderedDict()
-Bands4G['Three'] = ['9360', '6300', '1275', '1700', '525', '550']
+Bands4G['Three'] = ['9360', '6300', '1275', '1700', '525', '75']
 Bands4G['Eir'] = ['225', '1875', '6200', '9260']
-Bands4G['Vodafone'] = ['6400', '1450', '399']
+Bands4G['Vodafone'] = ['6400', '1450', '375', '9460']
